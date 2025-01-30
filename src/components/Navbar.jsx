b04b7143024403100601aebf344b05718de6d03a
lechatapaillettes <lucia.lazz@gmail.com>
2025-01-30 20:52:47 +0100

    responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+
+import { AiOutlineMenu } from "react-icons/ai";
+import { HiOutlineXMark } from "react-icons/hi2";
+import { Fade } from "react-awesome-reveal";
 
 const Navbar = () => {
   const [activeSection, setActiveSection] = useState("");
 
-  // Función para detectar la sección activa en el viewport
   const handleScroll = () => {
     const sections = document.querySelectorAll("section");
     let currentSection = "";
@@ -26,7 +30,6 @@ const Navbar = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  // Función para el scroll suave al hacer clic
   const handleNavLinkClick = (event, targetId) => {
     event.preventDefault();
     const targetElement = document.getElementById(targetId);
@@ -36,10 +39,27 @@ const Navbar = () => {
         top: targetElement.offsetTop,
       });
     }
+
+    setIsMenuOpen(false);
+  };
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isXMark, setIsXMark] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    setIsXMark(!isXMark);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+    setIsXMark(false);
   };
 
   return (
     <nav className="nav">
+      {/* Navbar Desktop */}
+
       <div className="nav-desktop">
         <ul className="nav-list">
           <li className="nav-item">
@@ -109,6 +129,89 @@ const Navbar = () => {
             </a>
           </li>
         </ul>
+      </div>
+
+      {/* Navbar Mobile */}
+
+      <div className="nav-mobile">
+        <div className="burger-menu">
+          <div className={`burger-icon-container ${isMenuOpen ? "open" : ""}`}>
+            <HiOutlineXMark
+              className={`burger-icon burger-icon-close ${
+                isMenuOpen ? "" : "burger-icon-hidden"
+              }`}
+              onClick={toggleMenu}
+            />
+            <AiOutlineMenu
+              className={`burger-icon burger-icon-open ${
+                isMenuOpen ? "burger-icon-hidden" : ""
+              }`}
+              onClick={toggleMenu}
+            />
+          </div>
+        </div>
+        {isMenuOpen && (
+          <Fade cascade damping={0.1}>
+            <ul className="burger-open">
+
+              <li className="burger-item">
+                <NavLink
+                  className="burger-link"
+                  to="/#accueil"
+                  onClick={(e) => handleNavLinkClick(e, "accueil", closeMenu)}
+                >
+                  <p>Accueil</p>
+                </NavLink>
+              </li>
+              <li className="burger-item">
+                <NavLink
+                  className="burger-link"
+                  to="/#psychomot"
+                  onClick={(e) => handleNavLinkClick(e, "psychomot", closeMenu)}
+                >
+                  <p>La psychomotricité</p>
+                </NavLink>
+              </li>
+              <li className="burger-item">
+                <NavLink
+                  className="burger-link"
+                  to="/#quandconsulter"
+                  onClick={(e) => handleNavLinkClick(e, "quandconsulter", closeMenu)}
+                >
+                  <p>Quand consulter ?</p>
+                </NavLink>
+              </li>
+              <li className="burger-item">
+                <NavLink
+                  className="burger-link"
+                  to="/#guidance"
+                  onClick={(e) => handleNavLinkClick(e, "guidance", closeMenu)}
+                >
+                  <p>Guidance Parentale</p>
+                </NavLink>
+              </li> 
+              <li className="burger-item">
+                <NavLink
+                  className="burger-link"
+                  to="/#quisuisje"
+                  onClick={(e) => handleNavLinkClick(e, "quisuisje", closeMenu)}
+                >
+                  <p>Qui suis-je ?</p>
+                </NavLink>
+              </li> 
+              <li className="burger-item">
+                <NavLink
+                  className="burger-link"
+                  to="/#contact"
+                  onClick={(e) => handleNavLinkClick(e, "contact", closeMenu)}
+                >
+                  <p>Contact</p>
+                </NavLink>
+              </li> 
+
+            </ul>
+          </Fade>
+        )}
       </div>
     </nav>
   );
